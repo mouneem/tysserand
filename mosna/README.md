8e38e6f022897292c476124cb107ae39a1f1ce54
mouneem <mounim1024@gmail.com>
2022-03-25 19:24:52 +0100

    update workflow

---
+++ mosna/README.md
@@ -0,0 +1,3 @@
+# mosna: multi-omics spatial network analysis
+
+A package to find patterns and communities in spatial networks of nodes with attributes of possibly high dimension.
---
+++ mosna/mosna/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+"""
+mosna: multi-omics spatial network analysis
+===============================================
+Documentation
+-------------
+"""
+
+
